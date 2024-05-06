2b7ab4b6bb3eb03592687a5127da3d2c4e65051e
tajbaba999 <tajbaba153@gmail.com>
2024-05-06 12:35:38 +0530

    signup page completed

---
+++ app/src/main/java/com/example/e_commerce_swipe/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.e_commerce_swipe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/e_commerce_swipe/activity/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.e_commerce_swipe.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.e_commerce_swipe.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
